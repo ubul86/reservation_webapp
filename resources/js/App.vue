0209093ad53c5f4b0ed6c885c0d52cd3a5ba3008
Sőrés Gábor <sores.gabor@evista.hu>
2024-08-23 16:24:48 +0200

    feature: initialize the project with authentication

---
+++ resources/js/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app>
+        <Header />
+    </v-app>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+    components: {
+        Header,
+    },
+    computed: {
+    },
+    data() {
+        return {
+        };
+    },
+    created() {
+        this.$store.dispatch("initializeUser");
+    },
+};
+</script>
+
+<style>
+@import "vuetify/dist/vuetify.min.css";
+</style>
